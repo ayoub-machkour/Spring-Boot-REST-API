96af9bd1ca267e7e898c6bd5a14b8c6b227d2e22
machkour <machkourayoub2002@gmail.com>
2024-09-29 06:40:13 +0100

    Full project: Spring Boot REST API for video library

---
+++ src/main/java/SpringBt/common/AppException.java
@@ -0,0 +1,16 @@
+package SpringBt.common;
+
+public class AppException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+    private String category;
+
+    public AppException(String category, String message) {
+        super(message);
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+}
